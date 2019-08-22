1bb3b0f9ce9de7c32ea04009f0550ba3659489af
Andrey Ponomarev <info@andreyponomarev.ru>
2019-08-22 18:04:15 +0300

    Add 'tooltip' component

--- src/index.js
+++ src/index.js
@@ -17,32 +17,20 @@ import './components/_reset.scss';
 /**************
  * COMPONENTS *
  **************/
+/* The order of components is from biggest to smallest. 
+
+Don't change their s precedence cause some of components rely on cascade so reordering may break the trules. For now, the only component that is really depends on cascade is 'tooltip'  -line-height property of 'tooltip' reassigns line-height propery of 'text' block */
+
 // demo
 import './components/demo/_demo.scss';
 
-//form
-//hamburger-menu
-
-//icon
-import './components/icons/_icons.scss';
-// breadcrumbs
-import './components/breadcrumbs/_breadcrumbs.scss';
-// label
-import './components/label/_label.scss';
-// backToTopBtn
-import './components/backToTopBtn/_backToTopBtn.scss';
-import './components/backToTopBtn/backToTopBtn.js';
-// blockquote
-import './components/blockquote/_blockquote.scss';
+// 'Global' blocks:
+// (blocks often serving as a parent container for other blocks)
 // page
 import './components/page/_page.scss';
 import './components/page/page.js';
 // grid
 import './components/grid/_grid.scss';
-// image-svg
-import './components/img-svg/_img-svg.scss';
-// image
-import './components/img-raster/_img-raster.scss';
 // header
 import './components/header/_header.scss';
 // text
@@ -55,9 +43,32 @@ import './components/list/_list.scss';
 import './components/btn/_btn.scss';
 // link
 import './components/link/_link.scss';
+// pagination
+import './components/pagination/_pagination.scss';
+
+// Small blocks:
+// (usually you don't put anything inside this blocks (except text))
+// image-svg
+import './components/img-svg/_img-svg.scss';
+// image
+import './components/img-raster/_img-raster.scss';
+// tooltip
+import './components/tooltip/_tooltip.scss';
+// icon
+import './components/icons/_icons.scss';
+// breadcrumbs
+import './components/breadcrumbs/_breadcrumbs.scss';
+// label
+import './components/label/_label.scss';
+// backToTopBtn
+import './components/backToTopBtn/_backToTopBtn.scss';
+import './components/backToTopBtn/backToTopBtn.js';
+// blockquote
+import './components/blockquote/_blockquote.scss';
 // code
 import './components/code/_code.scss';
 // hr
 import './components/hr/_hr.scss';
-// pagination
-import './components/pagination/_pagination.scss';
+
+//form
+//hamburger-menu
