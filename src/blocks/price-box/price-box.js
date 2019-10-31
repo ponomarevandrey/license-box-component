daef0ce2f09286f3ddc6f3a386bf9aeb240a8c9e
Andrey Ponomarev <info@andreyponomarev.ru>
2019-11-01 02:32:03 +0300

    Implement synch. between price plans, selectbox and 'total' node

--- src/blocks/price-box/price-box.js
+++ src/blocks/price-box/price-box.js
@@ -8,14 +8,6 @@ const box = document.querySelector('.box');
 
 //-
 
-// each time we change select box value or select another price plan (two event listeners) - the event fires:
-// - the event gets a) the currently selected price plan and it's price
-//                  b) the currently selected select box value
-//                  c) does the math
-//                  d) updates 'total'
-
-//-
-
 function renderBox(response) {
   const box__licensePlans = document.querySelector('.box__license-plans');
 
@@ -26,9 +18,10 @@ function renderBox(response) {
     control.classList.add('control', 'box__license-plan');
     control.setAttribute('for', id);
 
+    // create custom radio button (inactive)
     const control__customControl = document.createElement('div');
     control__customControl.classList.add('control__custom-control');
-
+    // create custom radio buttob (active)
     const control__customControl_active = document.createElement('div');
     control__customControl_active.classList.add(
       'control__custom-control_active'
@@ -56,27 +49,23 @@ function renderBox(response) {
     box__licensePlanPrice.classList.add('box__license-plan-price');
     box__licensePlanPrice.textContent = `${plan.price} per kicense`;
 
-    control.appendChild(control__customControl);
-    control.appendChild(control__systemControl);
-    control.appendChild(control__customControl_active);
-    control.appendChild(box__licensePlanName);
-    control.appendChild(box__licensePlanPrice);
-
+    control.append(
+      control__customControl,
+      control__systemControl,
+      control__customControl_active,
+      box__licensePlanName,
+      box__licensePlanPrice
+    );
     box__licensePlans.appendChild(control);
   }
 
   //-
 
   function selectPricePlan(e) {
-    console.log('jg');
+    // reset classes on all license plans:
     Array.from(document.querySelectorAll('.box__license-plan')).forEach(
       box__licensePlan => {
-        if (
-          box__licensePlan.classList.contains(
-            'box__license-plan_active'
-          ) /*&&
-          box__licensePlan.getAttribute('checked') === true*/
-        ) {
+        if (box__licensePlan.classList.contains('box__license-plan_active')) {
           box__licensePlan.classList.remove('box__license-plan_active');
           box__licensePlan
             .querySelector('input')
@@ -85,6 +74,7 @@ function renderBox(response) {
       }
     );
 
+    // get currently selected license plan:
     const [currentlySelectedPriceplan] = Array.from(this.children).filter(
       node => {
         const currentlySelectedRadioBtn = document.querySelector(
@@ -94,9 +84,8 @@ function renderBox(response) {
       }
     );
 
-    //const currentlySelectedPriceplan = e.target.closest('.box__license-plan');
+    // add style to currently selected license plan:
     currentlySelectedPriceplan.classList.add('box__license-plan_active');
-    //currentlySelectedPriceplan.classList.add('control__custom-control_active');
     currentlySelectedPriceplan
       .querySelector('input')
       .setAttribute('checked', true);
@@ -104,28 +93,31 @@ function renderBox(response) {
 
   //-
 
-  function renderSelectBox(numberOfLicenses) {
+  function renderSelectBox(numberOfLicenses, selectedByDefault) {
     const box__numberOfLicenses = document.querySelector(
       '.box__number-of-licenses'
     );
 
     const select = document.createElement('select');
-    select.classList.add('.box__selectbox');
+    select.classList.add('box__selectbox');
     select.id = 'number-of-licenses';
     select.name = 'number-of-licenses';
 
-    numberOfLicenses.forEach(number => {
+    numberOfLicenses.forEach((number, index) => {
       const option = document.createElement('option');
       option.value = number;
       option.textContent = number;
+
+      if (index === selectedByDefault - 1) {
+        option.setAttribute('selected', true);
+      }
+
       select.appendChild(option);
     });
 
     box__numberOfLicenses.appendChild(select);
 
-    select.addEventListener('change', handleSelectBox);
-
-    function handleSelectBox(e) {
+    function handleSelectBox() {
       const num = Number(this.options[this.selectedIndex].value);
       // const total = num *
 
@@ -138,16 +130,61 @@ function renderBox(response) {
             response.licensePlan[index].price.match(/\d+/)[0]
           );
           const total = num * planPrice;
-          console.log(total);
+          //console.log(total);
         }
       });
     }
+
+    select.addEventListener('change', handleSelectBox);
+
+    return select;
   }
 
-  response.licensePlan.forEach(renderPlan);
-  box__licensePlans.addEventListener('change', selectPricePlan);
+  function renderTotal() {
+    const box__total = document.querySelector('.box__total');
 
-  renderSelectBox(response.numberOfLicenses);
+    const currentSelectBoxValue = Number(
+      selectBox.options[selectBox.selectedIndex].value
+    );
+
+    const currentPrice = (() => {
+      const selectedPricePlan = document.querySelector(
+        '.box__license-plans input[type="radio"]:checked'
+      );
+      const [{ price }] = response.licensePlan.filter(
+        plan => plan.name === selectedPricePlan.value
+      );
+      return Number(price.match(/\d+/)[0]);
+    })();
+
+    box__total.textContent = `Total: ${currentPrice * currentSelectBoxValue}US`;
+  }
+
+  // render all license plans:
+  response.licensePlan.forEach(renderPlan);
+  // when user selects a price plan, evend handler updates the style of
+  // currently selected price plan and resets styles on all other plans:
+  box__licensePlans.addEventListener('change', selectPricePlan);
+  // build select box based on the data from JSON file and return it:
+  // (the second argument is the option selected by default)
+  const selectBox = renderSelectBox(response.numberOfLicenses, 2);
+
+  // if user changes price plan on selec box value, update 'box__total' div;
+  //
+  // the overall logic is the following: each time we change select box value
+  // or select another price plan, the 'renderTotal' event handler fires.
+  //
+  // The handler gets the:
+  // a) currently selected price plan and it's price value
+  // b) currently selected select box option
+  // c) does the math
+  // d) updates 'total' node displaying new sum
+  box__licensePlans.addEventListener('change', renderTotal);
+  selectBox.addEventListener('change', renderTotal);
+
+  // create synthetic event, to initialize price box with default values:
+  let init = new Event('change');
+  box__licensePlans.dispatchEvent(init);
 }
 
 //-
